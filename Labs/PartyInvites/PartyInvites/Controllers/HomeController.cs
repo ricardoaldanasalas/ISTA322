ca58f20af225bf844709f41146d72b0aa61d0cac
Ricardo Aldana Salas <ricardo.aldanasalas@outlook.com>
2020-03-02 08:34:45 -0500

    Add folder

---
+++ Labs/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Linq;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            //return "hello MSSA";
+            int hour = DateTime.Now.Hour;
+            //ternary operator
+            string greeting;
+            if (hour < 12)
+                greeting = "Good Morning";
+            else if (hour < 16)
+                greeting = "Good Afternoon";
+            else
+                greeting = "Good Evening";
+            ViewBag.Greeting = greeting;
+            ViewBag.Fairy = "Tinkerbel";
+            ViewBag.Cyborg = " Terminator";
+            //returns View();
+            return View("MyView");
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
